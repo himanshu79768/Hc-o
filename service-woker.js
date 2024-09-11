65a5c6046b46c37642a9a1bf7621d711bd56adbe
himanshu79768 <boranahiman45@gmail.com>
2024-09-11 12:10:45 +0530

    Update service-woker.js

--- service-woker.js
+++ service-woker.js
@@ -1,20 +1,23 @@
-const CACHE_NAME = 'my-pwa-cache-v1';
+
+const CACHE_NAME = 'pwa-test-cache-v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/icon.png'
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://s3.ezgif.com/tmp/ezgif-3-092ebae015.png',
+  'https://github.com/username/repo-name/raw/main/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });

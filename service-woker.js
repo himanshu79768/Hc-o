6732de56471f4fc6c88c2f0b7d6d28cb2b0d53a1
himanshu79768 <boranahiman45@gmail.com>
2024-09-11 12:21:53 +0530

    Update service-woker.js

--- service-woker.js
+++ service-woker.js
@@ -1,23 +1,37 @@
-
-const CACHE_NAME = 'pwa-test-cache-v1';
+const CACHE_NAME = 'my-pwa-cache-v2';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  'https://s3.ezgif.com/tmp/ezgif-3-092ebae015.png',
-  'https://github.com/username/repo-name/raw/main/icon-512x512.png'
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/https://s3.ezgif.com/tmp/ezgif-3-092ebae015.png'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
+
+self.addEventListener('push', event => {
+    const options = {
+        body: event.data.text(),
+        icon: 'icon.png',
+        vibrate: [100, 50, 100],
+        data: {
+            dateOfArrival: Date.now(),
+            primaryKey: 1
+        }
+    };
+
+    event.waitUntil(
+        self.registration.showNotification('Push Notification', options)
+    );
 });
